b58b7a808700be9a8c61a5926f7e208f09a4c7e5
RifqiPermanaPutra <rifqipermanaputra358@gmail.com>
2025-05-08 19:35:05 +0700

    Menambahkan statistik permainan untuk total, selesai, dan belum selesai

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,17 @@
       <h1>Game yang telah Ditamatkan</h1>
       <div class="underline"></div>
     </header>
-    <!-- Input Menambahkan  Game -->
+<!-- Input Menambahkan  Game -->
     <div class="input-group">
       <input v-model="newGame" placeholder="Tambahkan game" @keyup.enter="addGame" />
       <button @click="addGame"><span>Tambah</span> +</button>
     </div>
-     <!-- Filter Mana yg belum selesai -->
-     <div class="filter-buttons">
+ <!-- Filter Mana yg belum selesai -->
+    <div class="filter-buttons">
       <button :class="{ active: !showOnlyUnfinished }" @click="showOnlyUnfinished = false">Telah Tamat</button>
       <button :class="{ active: showOnlyUnfinished }" @click="showOnlyUnfinished = true">Belum Tamat</button>
     </div>
-    <!-- List Game -->
+   <!-- List Game -->
     <div class="list-area">
       <template v-if="filteredGames.length">
         <ul class="game-list">
@@ -32,8 +32,27 @@
 
         </ul>
       </template>
+
+      <div v-else class="empty-state">
+        <p>Belum ada Game yang ingin ditampilkan</p>
+      </div>
     </div>
+
+    <!--Bagian Status 3 dibawah-->
+    <div class="stats-container">
+  <div class="stat-box stat-total">
+    <div class="stat-title">Total Game</div>
+    <div class="stat-value">{{ games.length }}</div>
   </div>
+  <div class="stat-box stat-done">
+    <div class="stat-title">Tamat</div>
+    <div class="stat-value">{{ games.filter(g => g.finished).length }}</div>
+  </div>
+  <div class="stat-box stat-pending">
+    <div class="stat-title">Belum Tamat</div>
+    <div class="stat-value">{{ games.filter(g => !g.finished).length }}</div>
+  </div>
+</div>
   </div>
 </template>
 
