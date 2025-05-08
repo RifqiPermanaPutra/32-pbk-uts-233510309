fc1530084c44628d0cc0d5de376419c3286b8ba4
RifqiPermanaPutra <rifqipermanaputra358@gmail.com>
2025-05-08 18:56:14 +0700

    Input Untuk Menambahkan Game

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
       <h1>Game yang telah Ditamatkan</h1>
       <div class="underline"></div>
     </header>
+    <!-- Input Menambahkan  Game -->
+    <div class="input-group">
+      <input v-model="newGame" placeholder="Tambahkan game" @keyup.enter="addGame" />
+      <button @click="addGame"><span>Tambah</span> +</button>
+    </div>
   </div>
 </template>
 
