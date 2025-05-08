8667165b9903c74d447ea5b26259203c7ed31e24
RifqiPermanaPutra <rifqipermanaputra358@gmail.com>
2025-05-08 18:52:30 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dashboard">
+    <header class="header">
+      <h1>Game yang telah Ditamatkan</h1>
+      <div class="underline"></div>
+    </header>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+</script>
