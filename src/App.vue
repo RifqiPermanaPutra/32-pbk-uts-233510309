cbb070a3523d0cf77e0ea6269a3d73a23a21d1c1
RifqiPermanaPutra <rifqipermanaputra358@gmail.com>
2025-05-08 19:05:33 +0700

    Memfilter mana Game yang belum tamat

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,12 @@
       <input v-model="newGame" placeholder="Tambahkan game" @keyup.enter="addGame" />
       <button @click="addGame"><span>Tambah</span> +</button>
     </div>
+     <!-- Filter Mana yg belum selesai -->
+     <div class="filter-buttons">
+      <button :class="{ active: !showOnlyUnfinished }" @click="showOnlyUnfinished = false">Telah Tamat</button>
+      <button :class="{ active: showOnlyUnfinished }" @click="showOnlyUnfinished = true">Belum Tamat</button>
+    </div>
+    </div>
   </div>
 </template>
 
